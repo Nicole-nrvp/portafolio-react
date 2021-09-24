c83f06e189f462a9ac1d05263fffebe322d213a7
Nicole-nrvp <82890414+Nicole-nrvp@users.noreply.github.com>
2021-09-23 20:13:34 -0500

    Final Evidencia

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,13 +1,25 @@
+import {
+  Link
+} from "react-router-dom";
+
+
 function Error404(){
 
 	return(
 	<div className="error">
+    <div>
         <i class="fas fa-exclamation-triangle"></i>
         .
          <label className="textError">Error 404 Estás intentando acceder a una página que no existe</label>
         .
         <i class="fas fa-exclamation-triangle"></i>
       </div>
+
+      <div>
+        <h3 className="txtHabilidades"><Link to="/home">Volver</Link></h3>
+      </div>
+      </div>
+
 		);
 }
 
